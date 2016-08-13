b6a3733e083eb76a7901ee10be26b6c330bc44ee
omar <omarchebib@gmail.com>
2016-08-13 21:43:14 +0200

    Changing version number

--- Source/PowerPageComponent.cpp
+++ Source/PowerPageComponent.cpp
@@ -19,7 +19,7 @@ void PowerSpinnerTimer::timerCallback() {
 }
 
 unsigned char PowerPageComponent::rev_number = 8;
-unsigned char PowerPageComponent::bug_number = 2;
+unsigned char PowerPageComponent::bug_number = 5;
 
 PowerPageComponent::PowerPageComponent() {
   bgColor = Colours::black;
